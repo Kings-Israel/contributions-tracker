d7c1d33b4e3600daba36d2954e93bba31210bf22
Kings-Israel <milimokings@gmail.com>
2025-10-12 14:17:41 +0300

    Update

--- resources/js/components/AppSidebar.vue
+++ resources/js/components/AppSidebar.vue
@@ -5,7 +5,7 @@ import NavUser from '@/components/NavUser.vue';
 import { Sidebar, SidebarContent, SidebarFooter, SidebarHeader, SidebarMenu, SidebarMenuButton, SidebarMenuItem } from '@/components/ui/sidebar';
 import { type NavItem } from '@/types';
 import { Link } from '@inertiajs/vue3';
-import { BookOpen, Folder, LayoutGrid, DollarSign, Calculator } from 'lucide-vue-next';
+import { BookOpen, Folder, LayoutGrid, DollarSign, Calculator, Logs } from 'lucide-vue-next';
 import AppLogo from './AppLogo.vue';
 
 const mainNavItems: NavItem[] = [
@@ -28,6 +28,11 @@ const mainNavItems: NavItem[] = [
         title: 'Investments',
         href: '/investments',
         icon: LayoutGrid,
+    },
+    {
+        title: 'Logs',
+        href: '/logs',
+        icon: Logs,
     },
 ];
 
